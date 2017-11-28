2ef59cc045d06e6b807dd622da58050cd1db4ab2
Trevor Cickovski <tcickovs@fiu.edu>
2017-11-28 15:29:44 -0500

    Removed print statements

--- site_scons/site_tools/cuda/__init__.py
+++ site_scons/site_tools/cuda/__init__.py
@@ -88,7 +88,7 @@ def generate(env):
                 for path in paths:
                         if os.path.isdir(path):
                                 pathFound = True
-                                print 'scons: CUDA Toolkit found in ' + path
+                                #print 'scons: CUDA Toolkit found in ' + path
                                 cudaToolkitPath = path
                                 break
                 if not pathFound:
