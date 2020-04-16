00fcbe6d0bbd4169e7d5efa6539f167f76f7ba28
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-04-16 10:19:46 -0400

    Completing initial adoption of argparse

--- src/main.cxx
+++ src/main.cxx
@@ -52,8 +52,9 @@
 #include "languages/Py.h"
 #include "languages/R.h"
 
-#undef VERSION
+#ifndef VERSION
 #define VERSION "1.1.0"
+#endif
 
 #ifdef WITH_HEALTHCHECK
 #define WITH_HEALTHCHECK
@@ -62,6 +63,11 @@
 #define DEFAULT_PORT 3000
 #endif
 
+typedef struct {
+    char *config, *restart_point, *error_file, *log_file;
+    bool list;
+} arguments;
+
 const char *argp_program_version = VERSION;
 const char *argp_program_bug_address = "<tcickovs@fiu.edu>";
 char args_doc[] = "[CONFIG FILE] [<optional restart point>]";
@@ -69,14 +75,21 @@ static struct argp_option options[] = {
     {"log-file", 'l', "FILE", 0, "File to output logging information to. [default: ./logs/[TIMESTAMP].log.txt"},
     {"error-file", 'e', "FILE", 0, "File to output error information to. [default: /dev/stderr]"},
     {"healthcheck", 'H', 0, 0, "Enable the healthcheck socket. PluMA must be compiled with -DWITH_HEALTHCHECK. [default: false]"},
+    {"list", 'L', 0, 0, "Print a list of currently available plugins."},
     {0}
 };
 
-typedef struct {
-    char *args[2];
-    char *log_file, error_file;
-} arguments;
+/**
+    Parse the options for PluMA.
 
+    Arguments:
+        * int key - the current position of the key to access the arguments provided.
+        * char *arg - the value of the argument.
+        * argp_state - the state of the argument parser.
+
+    Returns:
+        0 or static error_t
+*/
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
     arguments *arguments = reinterpret_cast<::arguments *>(state->input);
 
@@ -85,7 +98,16 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
             if(state->arg_num >= 2) {
                 argp_usage(state);
             }
-            arguments->args[state->arg_num] = arg;
+            arguments->config = arg;
+            if (&state->argv[state->next]) {
+                try {
+                    arguments->restart_point = state->argv[state->next];
+                } catch (...) {
+                    perror("restart point error: ");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            state->next = state->argc;
             break;
         case ARGP_KEY_NO_ARGS:
             argp_usage(state);
@@ -102,18 +124,18 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 }
 
 static struct argp argp = { options, parse_opt, args_doc, 0 /* TODO: potential banner */ };
-
-////////////////////////////////////////////////////////////////////////////////
-// Helper Function: Convert int to string
-std::string toString(int val) {
-    std::string retval;
-    std::stringstream ss;
-    ss << val;
-    ss >> retval;
-    return retval;
-}
 ////////////////////////////////////////////////////////////////////////////////
 
+/**
+    Main executable.
+
+    Arguments:
+        * int argc - the number of arguments.
+        * char **argv - An array of arguments provided.
+
+    Return:
+        Exit code
+*/
 int main(int argc, char **argv) {
     ////////////////////////////////////////////////////////////////////////////
     // Display opening screen.
@@ -142,6 +164,7 @@ int main(int argc, char **argv) {
     std::cout << "*                     Bioinformatics 34(17):2881-2888, 2018                       *" << std::endl;
     std::cout << "*                                                                                 *" << std::endl;
     std::cout << "***********************************************************************************" << std::endl;
+    ////////////////////////////////////////////////////////////////////////////
 
     ////////////////////////////////////////////////////////////////////////////
     // Setup plugin path.
@@ -160,6 +183,7 @@ int main(int argc, char **argv) {
     std::map<std::string, std::string> pluginLanguages;
     supported.push_back(new Compiled("C", "so", pluginpath, "lib"));
     supported.push_back(new Py("Python", "py", pluginpath));
+    // TODO: Change prefix to 'PluMA'
     supported.push_back(new MiAMi::R("R", "R", pluginpath, argc, argv));
     supported.push_back(new Perl("Perl", "pl", pluginpath));
     ////////////////////////////////////////////////////////////////////////////
@@ -168,23 +192,32 @@ int main(int argc, char **argv) {
     // Command line arguments
     arguments arguments;
 
+    time_t t = time(0);
+    struct tm *now = localtime(&t);
+    std::string formatted_logfile = "logs/" + std::to_string(now->tm_year + 1900)
+        + "-" + std::to_string(now->tm_mon + 1) + "-"
+        + std::to_string(now->tm_mday) + "@" + std::to_string(now->tm_hour)
+        + ":" + std::to_string(now->tm_min) + ":" + std::to_string(now->tm_sec)
+        + ".log.txt";
+
+    arguments.log_file = (char *) formatted_logfile.c_str();
+    arguments.list = false;
+
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
-    ////////////////////////////////////////////////////////////////////////////
 
     ////////////////////////////////////////////////////////////////////////////
     // For each supported language, load the appropriate plugins
     std::string path = pluginpath.substr(0, pluginpath.find_first_of(":"));
-    bool list = false;
     while (path.length() > 0) {
         glob_t globbuf;
 
-        if (std::string(argv[1]) == "plugins") {
+        if (arguments.list) {
             std::cout << "[PluMA] Current plugin list: " << std::endl;
-            list = true;
         }
 
         for (int i = 0; i < supported.size(); i++) {
-            supported[i]->loadPlugin(path, &globbuf, &pluginLanguages, list);
+            supported[i]->loadPlugin(path, &globbuf, &pluginLanguages,
+                arguments.list);
         }
 
         std::map<std::string, std::string>::iterator itr;
@@ -202,41 +235,26 @@ int main(int argc, char **argv) {
         pluginpath = pluginpath.substr(pluginpath.find_first_of(":") + 1,
             pluginpath.length());
         path = pluginpath.substr(0, pluginpath.find_first_of(":"));
-    }
 
-    if (list) exit(EXIT_SUCCESS);
-    ////////////////////////////////////////////////////////////////////////////
-
-    ////////////////////////////////////////////////////////////////////////////
-    // Check for a restart point
-    bool doRestart = false;
-    std::string restartPoint = "";
-    if (argc == 3) {
-        doRestart = true;
-        restartPoint = std::string(argv[2]);
+        if (arguments.list) exit(EXIT_SUCCESS);
     }
     ////////////////////////////////////////////////////////////////////////////
 
     ////////////////////////////////////////////////////////////////////////////
-    // Get the current time, and setup the initial log file
-    time_t t = time(0);
-    struct tm *now = localtime(&t);
-    std::string currentTime = toString(now->tm_year + 1900) + "-"
-        + toString(now->tm_mon + 1) + "-" + toString(now->tm_mday) + "@"
-        + toString(now->tm_hour) + ":" + toString(now->tm_min) + ":"
-        + toString(now->tm_sec);
-    std::string mylog = "logs/"+ currentTime + ".log.txt";
-    PluginManager::getInstance().setLogFile(mylog);
+    // Setup the initial log file
+    // TODO: Should/Could this be a pointer?
+    PluginManager::getInstance().setLogFile(arguments.log_file);
 
 #ifdef WITH_HEALTHCHECK
     ////////////////////////////////////////////////////////////////////////////
     // Prepare thread for running the heartbeat socket.
     std::thread socket_thread(heartbeat, DEFAULT_PORT);
+    ////////////////////////////////////////////////////////////////////////////
 #endif
 
     ////////////////////////////////////////////////////////////////////////////
     // Read configuration file and make appropriate plugins
-    std::ifstream infile(argv[1], std::ios::in);
+    std::ifstream infile(arguments.config, std::ios::in);
     bool restartFlag = false;
     std::string prefix = "";
     while (!infile.eof()) {
@@ -266,8 +284,8 @@ int main(int argc, char **argv) {
         ////////////////////////////////////////////////////////////////////////
         // If we are restarting and have not hit that point yet,
         // skip this plugin
-        if (doRestart && !restartFlag) {
-            if (name == restartPoint) {
+        if (arguments.restart_point && !restartFlag) {
+            if (strcmp(name.c_str(), arguments.restart_point)) {
                 restartFlag = true;
             } else {
                 continue;
@@ -290,8 +308,9 @@ int main(int argc, char **argv) {
             ////////////////////////////////////////////////////////////////////
             // In this case we found the plugin, but the language
             // is not supported.
-            if (!executed && name != "")
+            if (!executed && name != "") {
                 PluginManager::getInstance().log("Error, no suitable language for plugin: " + name + ".");
+            }
             ////////////////////////////////////////////////////////////////////
 #ifdef WITH_HEALTHCHECK
 #endif
@@ -320,8 +339,9 @@ int main(int argc, char **argv) {
     // Wait for any socket use to be completed.
     socket_thread.join();
 #endif
-    for (int i = 0; i < supported.size(); i++)
+    for (int i = 0; i < supported.size(); i++) {
         supported[i]->unload();
+    }
     ////////////////////////////////////////////////////////////////////////////
 
     ////////////////////////////////////////////////////////////////////////////
