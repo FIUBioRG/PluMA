23d06abefb5f321086fcd5ce6a79054c529a2235
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-07-06 00:21:56 -0400

    Fixed to handle multiple hyperlinks in one table row

--- plugins/getPool.py
+++ plugins/getPool.py
@@ -12,6 +12,7 @@ while (page_source.find("</table>") != -1):
  # Individual Plugins
  plugins = plugin_table.split("<tr>")
  for plugin in plugins:
+  while(plugin.find("</a>") != -1):
    content = plugin[plugin.find("<a href="):plugin.find("</a>")]
    content = content.replace('<a href=', '')
    data = content.split('>')
@@ -21,5 +22,6 @@ while (page_source.find("</table>") != -1):
       else:
          repo = data[0][1:len(data[0])-1] # Remove quotes
          os.system("git clone "+repo)
+   plugin = plugin[plugin.find("</a>")+1:]
 
  page_source = page_source[page_source.find("</table>")+1:]
