041569ea094a698abe3bfe6d5ca2662fa02d7bc2
Joseph Quinn <423821+quinnjr@users.noreply.github.com>
2021-05-17 09:21:21 -0400

    Making getPool script always clone inside plugins directory

--- plugins/getPool.py
+++ plugins/getPool.py
@@ -16,12 +16,13 @@ while (page_source.find("</table>") != -1):
    content = plugin[plugin.find("<a href="):plugin.find("</a>")]
    content = content.replace('<a href=', '')
    data = content.split('>')
+   dirPath = os.path.dirname(os.path.realpath(__file__))
    if (len(data) == 2):
-      if (os.path.exists(data[1])):
+      if (os.path.exists(dirPath + "/" + data[1])):
          print("Plugin "+data[1]+" already installed.")
       else:
          repo = data[0][1:len(data[0])-1] # Remove quotes
-         os.system("git clone "+repo)
+         os.system("git clone " + repo + " " + dirPath + "/" + data[1])
    plugin = plugin[plugin.find("</a>")+1:]
 
  page_source = page_source[page_source.find("</table>")+1:]
