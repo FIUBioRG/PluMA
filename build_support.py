eb55f82e2eb44f2e304f97e9f46fcaa41c019c9e
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-08-15 18:47:03 -0400

    Upgrade to 2.0

---
+++ build_support.py
@@ -0,0 +1,84 @@
+#!python
+# Copyright (C) 2017, 2019-2020 FIUBioRG
+# SPDX-License-Identifier: MIT
+
+import os
+from os import path
+import subprocess
+from subprocess import Popen, PIPE
+import sys
+
+python_version = ".".join(map(str, sys.version_info[0:2]))
+
+###################################################################
+# HELPER FUNCTIONS
+###################################################################
+
+
+def SourcePath(*args):
+    res = []
+    for p in args:
+        res.append(path.abspath("./src/" + p))
+    return res
+
+
+def ObjectPath(*args):
+    res = []
+    for p in args:
+        res.append(path.abspath("./obj/" + p))
+    return res
+
+
+def LibPath(*args):
+    res = []
+    for p in args:
+        res.append(path.abspath("./lib/" + p))
+    return res
+
+
+def cmdline(command):
+    process = Popen(args=command, stdout=PIPE, shell=True)
+    return process.communicate()[0].decode("utf8")
+
+
+def CheckPerl(ctx):
+    ctx.Message("Checking Perl configuration... ")
+
+    source = """
+    use strict;
+    use Config;
+    use File::Spec;
+
+    sub search {
+        my $paths = shift;
+        my $file = shift;
+        foreach(@{$paths}) {
+            if (-f "$_/$file") {
+                return "$_/$file";
+                last
+            }
+        }
+        return;
+    }
+
+    my $coredir = File::Spec->catfile($Config{installarchlib}, "CORE");
+
+    open(F, ">", ".perlconfig.txt");
+    print F "perl=$Config{perlpath}\\n";
+    print F "typemap=" . search(\\@INC, "ExtUtils/typemap") . "\\n";
+    print F "xsubpp=" . search(\\@INC, "ExtUtils/xsubpp" || search([File::Spec->path()], "xsubpp")) . "\\n";
+    print F "coredir=$coredir\\n";
+    close F;
+    """
+
+    f = open(".perltest.pl", "w")
+    f.write(source)
+    f.close()
+
+    retcode = subprocess.call("perl .perltest.pl", shell=True)
+
+    ctx.Result(retcode == 0)
+
+    os.unlink(".perltest.pl")
+
+    return retcode == 0
