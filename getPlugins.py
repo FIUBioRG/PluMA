ab268b23f51d944824ba7ba82b1a92f46ac08ddc
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2022-06-10 14:36:44 -0400

    New website

--- getPlugins.py
+++ getPlugins.py
@@ -26,23 +26,63 @@ else:
    pluginpath = "../plugins"
 
 
+websites =set()
 # Plugin Table
 while (page_source.find("</table>") != -1):
  plugin_table = page_source[page_source.find("<table "):page_source.find("</table>")]
-
- # Individual Plugins
  plugins = plugin_table.split("<tr>")
+ count=0
  for plugin in plugins:
   while(plugin.find("</a>") != -1):
    content = plugin[plugin.find("<a href="):plugin.find("</a>")]
    content = content.replace('<a href=', '')
    data = content.split('>')
-   if (len(data) == 2 and data[1] in theplugins):
-      if (os.path.exists(pluginpath+"/"+data[1])):
-         print("Plugin "+data[1]+" already installed.")
-      else:
-         repo = data[0][1:len(data[0])-1] # Remove quotes
-         os.system("git clone "+repo+" "+pluginpath+"/"+data[1])
+   websites.add(data[0][1:len(data[0])-1])
    plugin = plugin[plugin.find("</a>")+1:]
-
  page_source = page_source[page_source.find("</table>")+1:]
+
+for website in websites:
+  response = urllib.request.urlopen("http://biorg.cis.fiu.edu/pluma/"+website)
+  page_source = str(response.read())
+  while (page_source.find("</table>") != -1):
+    plugin_table = page_source[page_source.find("<table "):page_source.find("</table>")]
+    # Individual Plugins
+    plugins = plugin_table.split("<tr>")
+    for plugin in plugins:
+     while(plugin.find("</a>") != -1):
+      content = plugin[plugin.find("<a href="):plugin.find("</a>")]
+      content = content.replace('<a href=', '')
+      data = content.split('>')
+      if (len(data) == 2 and data[1] in theplugins):
+         if (os.path.exists(pluginpath+"/"+data[1])):
+            print("Plugin "+data[1]+" already installed.")
+         else:
+            repo = data[0][1:len(data[0])-1] # Remove quotes
+            os.system("git clone "+repo+" "+pluginpath+"/"+data[1])
+      plugin = plugin[plugin.find("</a>")+1:]
+
+      #normalprintout(str(count)+" ", GREEN)
+      #print(count,end=" ")
+
+    page_source = page_source[page_source.find("</table>")+1:]
+
+# Plugin Table
+#while (page_source.find("</table>") != -1):
+# plugin_table = page_source[page_source.find("<table "):page_source.find("</table>")]
+#
+# # Individual Plugins
+# plugins = plugin_table.split("<tr>")
+# for plugin in plugins:
+#  while(plugin.find("</a>") != -1):
+#   content = plugin[plugin.find("<a href="):plugin.find("</a>")]
+#   content = content.replace('<a href=', '')
+#   data = content.split('>')
+#   if (len(data) == 2 and data[1] in theplugins):
+#      if (os.path.exists(pluginpath+"/"+data[1])):
+#         print("Plugin "+data[1]+" already installed.")
+#      else:
+#         repo = data[0][1:len(data[0])-1] # Remove quotes
+#         os.system("git clone "+repo+" "+pluginpath+"/"+data[1])
+#   plugin = plugin[plugin.find("</a>")+1:]
+#
+# page_source = page_source[page_source.find("</table>")+1:]
